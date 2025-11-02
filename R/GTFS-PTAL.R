f7cfb4f4897b9d86c58845b2d4d72e4e38654d4d
ChiaJung-Yeh <robert1328.mg10@nycu.edu.tw>
2025-11-02 22:20:36 +1100

    Initial commit

--- R/GTFS-PTAL.R
+++ R/GTFS-PTAL.R
@@ -2,62 +2,58 @@ library(dplyr)
 library(data.table)
 library(sf)
 library(stringr)
-library(ggplot2)
-library(xlsx)
 library(lubridate)
 
+usethis::use_package("dplyr")
+usethis::use_package("data.table")
+usethis::use_package("sf")
+usethis::use_package("stringr")
+usethis::use_package("lubridate")
 
-osm_data=read.csv("https://raw.githubusercontent.com/ChiaJung-Yeh/GTFS-PTAL/refs/heads/main/data/osm_region_geofabrik.csv")%>%
-  mutate(region=gsub("\\.", "", gsub("\\/", "-", region)))
-usethis::use_data(osm_data, overwrite=T)
+
+# osm_data=read.csv("https://raw.githubusercontent.com/ChiaJung-Yeh/PTAL/refs/heads/main/data/osm_region_geofabrik.csv")%>%
+#   mutate(region=gsub("\\.", "", gsub("\\/", "-", region)))%>%
+#   select(continent, region, subregion)
+# usethis::use_data(osm_data, overwrite=T)
 
 
 #### Download OSM Network ####
 #' @export
 OSM_Network=function(country, district=NULL){
+  osm_region=read.csv("https://raw.githubusercontent.com/ChiaJung-Yeh/PTAL/refs/heads/main/data/osm_region_geofabrik.csv")%>%
+    mutate(region=gsub("\\.", "", gsub("\\/", "-", region)))
+  DIRTEMP=tempdir(check=T)
+
+
   if(is.null(district)){
-    osm_region=filter(osm_region_all, region==country)
+    osm_region=filter(osm_region, region==country)
   }else{
-    osm_region=filter(osm_region_all, region==country, subregion==district)
+    osm_region=filter(osm_region, region==country, subregion==district)
   }
 
   if(nrow(osm_region)==0){
-    stop("Please use the valid country and district name! Please use '' to  check out the valid country and disteict name!")
+    stop("Please use the valid country and district name! Please use 'osm_data' to  check out the valid region (country) and subregion (district) name!")
   }
-  select(osm_region_all, continent, region, subregion)
-  usethis::use_data(TDX_County, overwrite=T)
 
-}
-
-
-for(i in c(1:nrow(osm_region))){
-  tempdir()
-  if(nrow(osm_region)==1){
-    download.file(osm_region$osm_pbf, paste0("Feature/OSM/", all_country[i], "/", osm_region$region, ".osm.pbf"), mode="wb")
+  for(i in c(1:nrow(osm_region))){
+    finame=ifelse(osm_region$subregion[i]!="", paste0(osm_region$region[i], "_", osm_region$subregion[i]), osm_region$region[i])
+    download.file(osm_region$osm_pbf[i], paste0(DIRTEMP, "/", finame, ".osm.pbf"), mode="wb")
     gdal_utils(
       util="vectortranslate",
-      source=paste0("Feature/OSM/", all_country[i], "/", osm_region$region, ".osm.pbf"),
-      destination=paste0("Feature/OSM/", all_country[i], "/", osm_region$region ,".gpkg"),
-      options = c("-f", "GPKG")
+      source=paste0(DIRTEMP, "/", finame, ".osm.pbf"),
+      destination=paste0(DIRTEMP, "/", finame,".gpkg"),
+      options = c("-f", "GPKG"),
+      quiet=T
     )
-  }else{
-    for(j in c(1:nrow(osm_region))){
-      dir.create(paste0("Feature/OSM/", all_country[i], "/", osm_region$subregion[j]))
-      if(grepl("russia", osm_region$osm_pbf[j])){
-        url=gsub("/europe/", "/", osm_region$osm_pbf[j])
-      }else{
-        url=osm_region$osm_pbf[j]
-      }
-      download.file(url, paste0("Feature/OSM/", all_country[i], "/", osm_region$subregion[j], "/", osm_region$subregion[j], ".osm.pbf"), mode="wb")
-      gdal_utils(
-        util="vectortranslate",
-        source=paste0("Feature/OSM/", all_country[i], "/", osm_region$subregion[j], "/", osm_region$subregion[j], ".osm.pbf"),
-        destination=paste0("Feature/OSM/", all_country[i], "/", osm_region$subregion[j], "/", osm_region$subregion[j] ,".gpkg"),
-        options = c("-f", "GPKG")
-      )
-    }
+    road_sf=st_read(paste0(DIRTEMP, "/", finame,".gpkg"), layer="lines")[, c("osm_id","name","highway","other_tags")]
   }
+
+  unlink(DIRTEMP, recursive=T)
+  return(road_sf)
 }
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# GTFS-PTAL
-# PTAL
 # PTAL
